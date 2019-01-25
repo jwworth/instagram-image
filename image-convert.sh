f5666ad6a1ed27559f910c4500fce828320bf413
Jake Worth <jake@jakeworth.com>
2019-01-24 19:53:41 -0600

    Final MVP comments

--- image-convert.sh
+++ image-convert.sh
@@ -1,18 +1,17 @@
-# 1. Whatever the size, resize so the largest side is 2000
+# Resize so the largest side is 2000
 magick "$1" -resize 2000x2000 output/resized.jpg
 
-# 2. Crop into an 1080x1080 square (need to be clearer on the position, will
-# this work on landscape)?
+# Crop (1080x1080)
 magick output/resized.jpg -crop 1080x1080+0+460 output/cropped.jpg
 
-# 3. Darken image
+# Darken image
 magick convert output/cropped.jpg -fill black -colorize 40% output/darkened.jpg
 
-# 4. Put logo into overlay
+# Superimpose logo
 magick composite -compose atop -geometry +100+100 assets/logo.png output/darkened.jpg output/with-logo.jpg
 
-# 5. Add Hashrocket logo
+# Add header
 magick convert output/with-logo.jpg -fill white -font fonts/CircularStd-Book.otf -pointsize 100 -annotate +100+500 'Blog | Hashrocket' output/with-title.jpg
 
-# 6. Add blog title
+# Add blog title
 magick convert output/with-title.jpg -fill white -font fonts/CircularStd-Bold.otf -pointsize 100 -annotate +100+630 "$2" output/ready-for-instagram.jpg

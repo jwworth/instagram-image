f44ba931832fb3e0cf103aca1eda90b3669f6ec9
Jake Worth <jake@jakeworth.com>
2019-01-28 17:49:42 -0600

    Rename temp dir to convention

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ post '/download' do
   background = params[:file][:tempfile]
   title = params[:title]
   `./image-convert.sh #{background.path} "#{title}"`
-  result = File.new('output/ready-for-instagram.jpg')
+  result = File.new('tmp/ready-for-instagram.jpg')
 
   send_file result.path
 end
--- image-convert.sh
+++ image-convert.sh
@@ -3,18 +3,18 @@ magick "$1" \
  -resize 2000x2000 \
  -crop 1080x1080+0+460 \
  -fill black -colorize 40% \
- output/resized-cropped-darkened.jpg
+ tmp/resized-cropped-darkened.jpg
 
 # Superimpose logo
 magick composite \
   -compose atop -geometry +100+100 \
-  assets/logo.png output/resized-cropped-darkened.jpg \
-  output/with-logo.jpg
+  assets/logo.png tmp/resized-cropped-darkened.jpg \
+  tmp/with-logo.jpg
 
 # Add header and title
 magick convert \
-  output/with-logo.jpg \
+  tmp/with-logo.jpg \
   -fill white -font fonts/CircularStd-Book.otf \
   -pointsize 100 -annotate +100+500 'Blog | Hashrocket' \
   -fill white -font fonts/CircularStd-Bold.otf \
-  -pointsize 100 -annotate +100+630 "$2" output/ready-for-instagram.jpg
+  -pointsize 100 -annotate +100+630 "$2" tmp/ready-for-instagram.jpg

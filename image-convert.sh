16ccc19d9452f6a9b7f69d41b05e25427938dea4
Jake Worth <jake@jakeworth.com>
2019-01-28 17:33:10 -0600

    Merge repetitive actions

--- image-convert.sh
+++ image-convert.sh
@@ -1,19 +1,20 @@
-# Resize so the largest side is 2000
-magick "$1" -resize 2000x2000 output/resized.jpg
-
-# Crop (1080x1080)
-magick output/resized.jpg -crop 1080x1080+0+460 output/cropped.jpg
-
-# Darken image
-magick convert output/cropped.jpg -fill black -colorize 40% output/darkened.jpg
+# Resize, crop, darken
+magick "$1" \
+ -resize 2000x2000 \
+ -crop 1080x1080+0+460 \
+ -fill black -colorize 40% \
+ output/resized-cropped-darkened.jpg
 
 # Superimpose logo
-magick composite -compose atop -geometry +100+100 assets/logo.png output/darkened.jpg output/with-logo.jpg
+magick composite \
+  -compose atop -geometry +100+100 \
+  assets/logo.png output/resized-cropped-darkened.jpg \
+  output/with-logo.jpg
 
-# Add header
-magick convert output/with-logo.jpg -fill white -font fonts/CircularStd-Book.otf \
-  -pointsize 100 -annotate +100+500 'Blog | Hashrocket' output/with-title.jpg
-
-# Add blog title
-magick convert output/with-title.jpg -fill white -font fonts/CircularStd-Bold.otf \
+# Add header and title
+magick convert \
+  output/with-logo.jpg \
+  -fill white -font fonts/CircularStd-Book.otf \
+  -pointsize 100 -annotate +100+500 'Blog | Hashrocket' \
+  -fill white -font fonts/CircularStd-Bold.otf \
   -pointsize 100 -annotate +100+630 "$2" output/ready-for-instagram.jpg

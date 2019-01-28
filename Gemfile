228f937d32c7ce2592a1f2eefe06c96bb7be63ef
Jake Worth <jake@jakeworth.com>
2019-01-28 14:16:55 -0600

    Add web server

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source 'https://rubygems.org'
 ruby '2.6.0'
 
 gem 'thor'
+gem 'sinatra'
---
+++ app.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+
+get '/' do
+  erb :form
+end
+
+post '/download' do
+  background = params[:file][:tempfile]
+  title = params[:title]
+  `./image-convert.sh #{background.path} "#{title}"`
+  result = File.new('output/ready-for-instagram.jpg')
+
+  send_file result.path
+end

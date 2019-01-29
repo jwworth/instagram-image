70e8256dc06b1ebfd66d852f2b4821db059b6a21
Jake Worth <jake@jakeworth.com>
2019-01-28 22:43:26 -0600

    Use pipes!

--- image-convert.sh
+++ image-convert.sh
@@ -3,17 +3,17 @@ magick "$1" \
  -resize 2000x2000 \
  -crop 1080x1080+0+460 \
  -fill black -colorize 40% \
- tmp/resized-cropped-darkened.jpg
+ jpg:- | \
 
 # Superimpose logo
 magick composite \
   -compose atop -geometry +100+100 \
-  assets/logo.png tmp/resized-cropped-darkened.jpg \
-  tmp/with-logo.jpg
+  assets/logo.png jpg:- \
+  jpg:- | \
 
 # Add header and title
 magick convert \
-  tmp/with-logo.jpg \
+  jpg:- \
   -fill white -font fonts/CircularStd-Book.otf \
   -pointsize 100 -annotate +100+500 'Blog | Hashrocket' \
   -fill white -font fonts/CircularStd-Bold.otf \

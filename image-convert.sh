61ff94872e3edc059604e37844745a57a15afa3f
Jake Worth <jake@jakeworth.com>
2019-01-30 15:17:18 -0600

    Don't directly use `magick`

--- image-convert.sh
+++ image-convert.sh
@@ -2,20 +2,20 @@
 set -eu -o pipefail
 
 # Resize, crop, darken
-magick - \
+convert - \
   -resize 2000x2000 \
   -crop 1080x1080+0+460 \
   -fill black -colorize 40% \
   jpg:- |
 
 # Superimpose logo
-magick composite \
+composite \
   -compose atop -geometry +100+100 \
   assets/logo.png \
   jpg:- jpg:- |
 
 # Add header and title
-magick convert \
+convert \
   -fill white -font fonts/CircularStd-Book.otf \
   -pointsize 100 -annotate +100+500 'Blog | Hashrocket' \
   -fill white -font fonts/CircularStd-Bold.otf \

98273e2e736d61853f4beaa0006c702459445c49
Jake Worth <jake@jakeworth.com>
2019-01-30 10:40:13 -0600

    Add a title generation function

--- app.rb
+++ app.rb
@@ -7,16 +7,24 @@ end
 
 post '/download' do
   background = params[:file][:tempfile]
+  title = params[:title]
+
   output, _error, status = Open3.capture3(
     "./image-convert.sh",
-    params[:title],
+    title,
     stdin_data: background.read,
     binmode: true
   )
 
   if status.success?
     content_type('image/jpeg')
-    attachment('test.jpg')
+    attachment(generate_title(title))
     output
   end
+end
+
+private
+
+def generate_title(title)
+  title.downcase.gsub(/\s+/, "_") + "_#{Time.now.to_i}.jpg"
 end
